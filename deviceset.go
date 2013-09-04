de07f409734115cb2511f0ec8aba4dfc69c5d54e
Alexander Larsson <alexl@redhat.com>
2013-09-04 11:44:11 +0200

    Add DeviceSet interface
    This interface matches the device-mapper implementation (DeviceSetDM)
    but is free from any dependencies. This allows core docker code
    to refer to a DeviceSet without having an explicit dependency on
    the devmapper package.
    
    This is important, because the devmapper package has external
    dependencies which are not wanted in the docker client app, as it
    needs to run with minimal dependencies in the docker image.

---
+++ deviceset.go
@@ -0,0 +1,13 @@
+package docker
+
+type DeviceSet interface {
+	AddDevice(hash, baseHash string) error
+	SetInitialized(hash string) error
+	DeactivateDevice(hash string) error
+	RemoveDevice(hash string) error
+	MountDevice(hash, path string) error
+	HasDevice(hash string) bool
+	HasInitializedDevice(hash string) bool
+}
+
+type DeviceSetFactory func(string) (DeviceSet, error)
