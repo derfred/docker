665452b74361ace91df54c153594a8c303a301a8
Alexander Larsson <alexl@redhat.com>
2014-02-18 11:58:38 +0100

    aufs: Skip tests if aufs not supported
    This makes it possible to pass the rest of the testsuite without aufs in the kernel.
    
    Docker-DCO-1.1-Signed-off-by: Alexander Larsson <alexl@redhat.com> (github: alexlarsson)

--- graphdriver/aufs/aufs.go
+++ graphdriver/aufs/aufs.go
@@ -34,6 +34,12 @@ import (
 	"sync"
 )
 
+var (
+	ErrAufsNotSupported = fmt.Errorf("AUFS was not found in /proc/filesystems")
+)
+
+var err error
+
 func init() {
 	graphdriver.Register("aufs", Init)
 }
@@ -100,7 +106,7 @@ func supportsAufs() error {
 			return nil
 		}
 	}
-	return fmt.Errorf("AUFS was not found in /proc/filesystems")
+	return ErrAufsNotSupported
 }
 
 func (a Driver) rootPath() string {
